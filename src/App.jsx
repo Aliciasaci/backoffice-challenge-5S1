79e839b1042a1c1ad88d109e0294b9309024a813
Alicia Saci <lyciiasci@gmail.com>
2024-02-15 01:43:02 +0100

    Test modification route pour deploy

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
                   }
                 >
                   <Route
-                    path="/backoffice-challenge-5S1/"
+                    path="/"
                     element={
                       <Layout>
                         <DashboardWrapper />
@@ -46,7 +46,7 @@ function App() {
                 </Route>
                 <Route element={<RequireAuth allowedRoles={["ROLE_ADMIN"]} />}>
                   <Route
-                    path="/backoffice-challenge-5S1/admin/dashboard"
+                    path="/admin/dashboard"
                     element={
                       <Layout>
                         <AdminDashboard />
@@ -54,7 +54,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/backoffice-challenge-5S1/admin/users"
+                    path="/admin/users"
                     element={
                       <Layout>
                         <CrudUser />
@@ -62,7 +62,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/backoffice-challenge-5S1/admin/categories"
+                    path="/admin/categories"
                     element={
                       <Layout>
                         <CrudCategory />
@@ -70,7 +70,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/backoffice-challenge-5S1/admin/etablissements"
+                    path="/admin/etablissements"
                     element={
                       <Layout>
                         <AdminEtablissement />
@@ -78,7 +78,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/backoffice-challenge-5S1/admin/demandes"
+                    path="/admin/demandes"
                     element={
                       <Layout>
                         <ValidationEtablissement />
@@ -90,7 +90,7 @@ function App() {
                   element={<RequireAuth allowedRoles={["ROLE_PRESTATAIRE"]} />}
                 >
                   <Route
-                    path="/backoffice-challenge-5S1/prestataire/employes"
+                    path="/prestataire/employes"
                     element={
                       <Layout>
                         <CrudEmploye />
@@ -98,7 +98,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/backoffice-challenge-5S1/prestataire/prestations"
+                    path="/prestataire/prestations"
                     element={
                       <Layout>
                         <CrudPrestation />
@@ -106,7 +106,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/backoffice-challenge-5S1/prestataire/etablissements"
+                    path="/prestataire/etablissements"
                     element={
                       <Layout>
                         <CrudEtablissement />
@@ -114,7 +114,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/backoffice-challenge-5S1/prestataire/reservations"
+                    path="/prestataire/reservations"
                     element={
                       <Layout>
                         <HistoriqueReservation />
@@ -124,7 +124,7 @@ function App() {
                 </Route>
               </Route>
               <Route
-                path="/backoffice-challenge-5S1/login"
+                path="/login"
                 element={
                   <SimpleLayout>
                     <LogIn />
